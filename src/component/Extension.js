0c9c8313d2f7ecee46164ad0e94aad30f673ea92
yuno <hanganda23@naver.com>
2021-07-19 23:28:37 +0900

    restExtension

--- src/component/Extension.js
+++ src/component/Extension.js
@@ -8,9 +8,10 @@ const exploreContent = [
     {title:'미디어',subContent:['콘텐츠 크리에이터','PD','영상 편집가','에디터','비디오 제작','작가','출판 기획자','더보기']},
     {title:'인사',subContent:['인사담당','리크루터','조직문화','평가·보상','헤드헌터','HRBP','HRD','더보기']},
     {title:'게임 제작',subContent:['게임 기획자','게임 그래픽 디자이너','모바일 게임 개발자','게임 아티스트','게임 클라이언트 개발자','게임 서버 개발자','유니티 개발자','더보기']},
-    {title:'금융',subContent:['회계담당','재무담당자','IR','투자 증권','재무 분석가','애널리스트','내부통제 담당자','더보기']},
+    {title:'금융',subContent:['회계담당','재무담당자','IR','투자·증권','재무 분석가','애널리스트','내부통제 담당자','더보기']},
 ]
 
+const restExploreContent = ['엔지니어링·설계','제조·생산','물류·무역','의료·제약·바이오','교육','법률·법집행기관','식·음료','건설·시설','공공·복지']
 
 function Extension({showId, hideExtension:hide}) {
     
@@ -29,12 +30,25 @@ function Extension({showId, hideExtension:hide}) {
         />)
     }
 
+    const renderRestContent = ()=> {
+        return restExploreContent.map((content,index)=>
+            <li key={index}>{content}</li>
+        )
+    }
+
     return (
         showId ? 
         <div className={OVERLAY_CLASSNAME} onMouseOver={onMouseOver}>
             <div className='extension'>
                 <div className='container'>
                     {renderContent()}
+                    
+                    <div className='explore-field'>
+                        <ul className='rest-list'>
+                            {renderRestContent()}
+                        </ul>
+                    </div>
+
                 </div>
             </div>
         </div>
--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -5,10 +5,10 @@ function Navigation() {
         <ul className='nav-list'>
             <li data-show-id='explore'>탐색</li>
             <li data-show-id=''>커리어 성장</li>
-            <li data-show-id=''>직군별 연봉</li>
-            <li data-show-id=''>이력서</li>
-            <li data-show-id=''>매치업</li>
-            <li data-show-id=''>프리랜서</li>
+            <li data-show-id='' className='mobile-hide'>직군별 연봉</li>
+            <li data-show-id='' className='mobile-hide'>이력서</li>
+            <li data-show-id='' className='mobile-hide'>매치업</li>
+            <li data-show-id='' className='mobile-hide'>프리랜서</li>
         </ul>
     )
 }
