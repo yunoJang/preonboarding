a8d44c172f8ed56a15fcbd220ea2860fe14e32b5
yuno <hanganda23@naver.com>
2021-07-20 02:38:33 +0900

    submit

--- src/component/Extension.js
+++ src/component/Extension.js
@@ -14,7 +14,7 @@ const exploreContent = [
 const restExploreContent = ['엔지니어링·설계','제조·생산','물류·무역','의료·제약·바이오','교육','법률·법집행기관','식·음료','건설·시설','공공·복지']
 
 function Extension({showId, hide}) {
-    
+
     const onMouseOver = event=> {
         if(event.target.classList.contains(OVERLAY_CLASSNAME)) {
             hide();
@@ -29,6 +29,7 @@ function Extension({showId, hide}) {
             subContent = {column.subContent}
         />)
     }
+    
     return (
         showId ? 
         <div className={OVERLAY_CLASSNAME} onMouseOver={onMouseOver}>
